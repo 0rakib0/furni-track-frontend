4236b96c1073481460d188c8c25a28e867370c75
rakib00 <hassanrakibul926@gmail.com>
2025-08-02 01:31:31 +0600

    create add order rout and design form

--- src/app/(orders)/add-order/page.jsx
+++ src/app/(orders)/add-order/page.jsx
@@ -6,7 +6,7 @@ function AddOrder() {
         <div>
             <PageTitle></PageTitle>
             <div className='bg-white p-4 md:w-3/4 md:m-auto rounded-lg'>
-            <h3 className='text-2xl text-center '>➕Add Order</h3>
+                <h3 className='text-2xl text-center '>➕Add Order</h3>
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend text-lg font-thin">Customar Name</legend>
                     <input type="text" className="input w-full focus:outline-none" placeholder="Customar Name" />
@@ -25,7 +25,7 @@ function AddOrder() {
                 </fieldset>
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend text-lg font-thin">Delivery Date</legend>
-                    <input type="date" className="input w-full focus:outline-none"/>
+                    <input type="date" className="input w-full focus:outline-none" />
                 </fieldset>
                 <fieldset className="fieldset">
                     <legend className="fieldset-legend text-lg font-thin">Frame Show Date</legend>
@@ -35,6 +35,18 @@ function AddOrder() {
                     <legend className="fieldset-legend text-lg font-thin">Select Dealer</legend>
                     <input type="text" className="input w-full focus:outline-none" placeholder="Frame Show Date" />
                 </fieldset>
+                <fieldset className="fieldset">
+                    <legend className="fieldset-legend text-lg font-thin">Select Employee</legend>
+                    <input type="text" className="input w-full focus:outline-none" placeholder="Frame Show Date" />
+                </fieldset>
+                <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-ful border p-4">
+                    <legend className="fieldset-legend">Delivery Status</legend>
+                    <label className="label">
+                        <input type="checkbox" defaultChecked className="checkbox" />
+                        delivered/pending
+                    </label>
+                </fieldset>
+                <button type='submit' className='w-full py-3 rounded-md text-white hover:bg-white hover:border hover:text-[#57c7d4] bg-[#57c7d4] transition duration-300 ease-in-out'>Add Order</button>
             </div>
         </div>
     )
