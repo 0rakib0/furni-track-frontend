ec27cc5e1bec611abb6a660366a593bcaae73843
rakib00 <hassanrakibul926@gmail.com>
2025-08-28 02:08:42 +0600

    swt modal for view single order

--- src/components/orderComponents/OrdersTable.jsx
+++ src/components/orderComponents/OrdersTable.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { FaRegEye, FaPencilAlt } from "react-icons/fa";
+import { ImBin } from "react-icons/im";
+import ViewSingleOrder from './ViewSingleOrder';
 
 function OrdersTable({ title, orders }) {
+    const [selectOrder, setSelectOrder] = useState(null)
 
+    const handleViewSingleOrder = order =>{
+        setSelectOrder(order)
+        document.getElementById('viewSingleOrder').showModal()
+    }
 
 
     return (
@@ -23,20 +32,33 @@ function OrdersTable({ title, orders }) {
                     <tbody>
 
                         {
-                            orders.map(order => 
+                            orders.map(order =>
                                 <tr key={order.id}>
                                     <th>{order.id}</th>
                                     <td>{order.memo_number}</td>
                                     <td>{order.customar_name}</td>
                                     <td>{order.delivery_date}</td>
-                                    <span className={`badge badge-soft ${order.order_status ? 'bg-green-500 text-white' :'bg-red-500 text-white'}`}>{order.order_status ? "Delivered":"Pending"}</span>
-                                    <td>Blue</td>
+                                    <td>
+                                        <span className={`badge badge-soft ${order.order_status ? 'bg-green-500 text-white' : 'bg-red-500 text-white'}`}>{order.order_status ? "Delivered" : "Pending"}</span>
+                                    </td>
+                                    <td>
+                                        <button onClick={()=>handleViewSingleOrder(order)} className="badge badge-soft badge-info mr-2"><span className='text-xl'><FaRegEye /></span>
+                                        </button>
+
+                                        <button className="badge badge-soft text-red-400 mr-2">
+                                            <span><ImBin></ImBin></span>
+                                        </button>
+
+                                        <button className="badge badge-soft badge-primary mr-2">
+                                            <span> <FaPencilAlt></FaPencilAlt> </span></button>
+                                    </td>
                                 </tr>
                             )
                         }
-                
+
                     </tbody>
                 </table>
+                <ViewSingleOrder order={selectOrder}></ViewSingleOrder>
             </div>
         </div>
     )
