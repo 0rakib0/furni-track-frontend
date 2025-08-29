0c08fed8e6efd266eb6a897ada69c6c52416c5b9
rakib00 <hassanrakibul926@gmail.com>
2025-08-30 01:55:37 +0600

    delivery update status changed

--- src/components/orderComponents/OrderUpdate.jsx
+++ src/components/orderComponents/OrderUpdate.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 
 function OrderUpdate({ order }) {
@@ -8,6 +8,15 @@ function OrderUpdate({ order }) {
     const [loading, setLoading] = useState(false)
     const orderId = order?.id
     const router = useRouter()
+
+    const [deliveryStatus, setDeliveryStatus] = useState(false);
+
+    useEffect(() => {
+        if (order?.order_status !== undefined) {
+            setDeliveryStatus(order.order_status);
+        }
+    }, [order]);
+
     const handleFormSubmit = event => {
         event.preventDefault()
         setLoading(true)
@@ -19,6 +28,7 @@ function OrderUpdate({ order }) {
         const deliveryAdress = form.delivery_address.value;
         const deliveryDate = form.delivery_date.value;
         const frameShowDate = form.frame_show_date.value;
+        const delivery_status = form.delivery_status.value;
 
         const orderFormData = new FormData()
 
@@ -28,6 +38,7 @@ function OrderUpdate({ order }) {
         orderFormData.append('delivery_address', deliveryAdress)
         orderFormData.append('delivery_date', deliveryDate)
         orderFormData.append('frame_show_date', frameShowDate)
+        orderFormData.append('order_status', delivery_status)
 
         fetch(`http://127.0.0.1:8000/orders/${orderId}/`, {
             method: "PATCH",
@@ -92,6 +103,20 @@ function OrderUpdate({ order }) {
                                 <legend className="fieldset-legend text-lg font-thin">Frame Show Date</legend>
                                 <input type="date" name='frame_show_date' className="input w-full focus:outline-none" defaultValue={order?.frame_show_date} />
                             </fieldset>
+                            <fieldset className="fieldset bg-base-100 border-base-300 rounded-box w-ful border p-4">
+                                <legend className="fieldset-legend">Delivery Status</legend>
+                                <label className="label">
+                                    <input
+                                        type="checkbox"
+                                        name='delivery_status'
+                                        className="checkbox"
+                                        checked={deliveryStatus}
+                                        onChange={(e) => setDeliveryStatus(e.target.checked)}
+                                    />
+                                    delivered/pending
+                                </label>
+                            </fieldset>
+
                             <button type='submit' className='w-full py-3 rounded-md text-white hover:bg-white hover:border hover:text-[#57c7d4] bg-[#57c7d4] transition duration-300 ease-in-out my-4'>
                                 {loading ? <span className="loading loading-bars loading-lg"></span> : 'Update Order'}
                             </button>
