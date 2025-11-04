e0cbb9f52b97e371f0043f316b9e44db8cb84e5d
rakib00 <hassanrakibul926@gmail.com>
2025-11-04 08:56:17 +0600

    get order dates from backend and show it in calneder

--- src/components/DeliveryCalender/DeliveryCalendar.jsx
+++ src/components/DeliveryCalender/DeliveryCalendar.jsx
@@ -1,11 +1,33 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./calendar.css"; // custom CSS file
 
 export default function DeliveryCalendar() {
-    const deliveryDates = ["2025-11-28", "2025-11-10", "2025-11-15", "2025-11-30"];
+
+    const [deliveryDates, setDeliveryDate] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(()=>{
+        const fetchOrderDates = async () =>{
+            try{
+                setLoading(true)
+                const res = await fetch("http://127.0.0.1:8000/delivery-dates")
+                const dates = await res.json()
+                setDeliveryDate(dates)
+            }catch(err){
+                setError(err.mesaage)
+            }finally{
+                setLoading(false)
+            }
+        }
+        fetchOrderDates()
+    }, [])
+
+    console.log(deliveryDates)
+    // const deliveryDates = ["2025-11-28", "2025-11-10", "2025-11-15", "2025-11-30"];
 
     const tileClassName = ({ date, view }) => {
         if (view === "month") {
@@ -18,6 +40,8 @@ export default function DeliveryCalendar() {
     };
 
 
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p className='text-red-500'>Error: {error}</p>
     return (
         <div>
             <h2 className="text-2xl font-semibold mb-4">Delivery Calendar</h2>
