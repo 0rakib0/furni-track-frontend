ca019febce66642197dad5074d0fcb722dfe3d80
rakib00 <hassanrakibul926@gmail.com>
2025-11-09 09:06:11 +0600

    design payment report table for show payment report

--- src/app/(dealer)/dealer-payment-report/page.jsx
+++ src/app/(dealer)/dealer-payment-report/page.jsx
@@ -1,9 +1,12 @@
+import PaymentReport from '@/components/dealerComponents/PaymentReport/PaymentReport'
+import PageTitle from '@/components/PageTitle/PageTitle'
 import React from 'react'
 
 function DealerPaymentReport() {
   return (
     <div>
-        All Dealer Payment report in here!
+        <PageTitle></PageTitle>
+        <PaymentReport></PaymentReport>
     </div>
   )
 }
---
+++ src/components/dealerComponents/PaymentReport/PaymentReport.jsx
@@ -0,0 +1,52 @@
+import DeleteAlart from '@/components/deleteConfirmationAlert/DeleteAlart'
+import React from 'react'
+import { FaPencilAlt, FaRegEye } from 'react-icons/fa'
+import { ImBin } from 'react-icons/im'
+
+function PaymentReport() {
+    return (
+        <div>
+            <div className='my-6'>
+                <h3 className='md:text-2xl text-xl mb-2 text-[#57c7d4]'>🧾Payment List</h3>
+                <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 shadow-lg">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Dealer Name</th>
+                                <th>Ref Memo</th>
+                                <th>Payment Method</th>
+                                <th>Amount</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>Rakib</td>
+                                <td>837</td>
+                                <td>Online Bank Trancfer</td>
+                                <td>50000</td>
+                                <td>
+                                    <button className="badge badge-soft badge-info mr-2"><span className='text-xl'><FaRegEye /></span>
+                                    </button>
+
+                                    <button className="badge badge-soft text-red-400 mr-2">
+                                        <span><ImBin></ImBin></span>
+                                    </button>
+                                    <DeleteAlart status='dealer'></DeleteAlart>
+
+                                    <button className="badge badge-soft badge-primary mr-2">
+                                        <span> <FaPencilAlt></FaPencilAlt> </span></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PaymentReport
