328a0f9474d370b304b38e78d40538c34275515a
rakib00 <hassanrakibul926@gmail.com>
2025-11-19 00:29:46 +0600

    create complain list rout and components

---
+++ src/app/(customar)/complain-list/page.jsx
@@ -0,0 +1,14 @@
+import ViewCustomarComplain from '@/components/customarComponent/ViewCustomarComplain'
+import PageTitle from '@/components/PageTitle/PageTitle'
+import React from 'react'
+
+function ComplainList() {
+  return (
+    <div>
+        <PageTitle></PageTitle>
+        <ViewCustomarComplain></ViewCustomarComplain>
+    </div>
+  )
+}
+
+export default ComplainList
---
+++ src/components/customarComponent/ViewCustomarComplain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ViewCustomarComplain() {
+  return (
+    <div>
+        <h1>Custopmar comlain view</h1>
+    </div>
+  )
+}
+
+export default ViewCustomarComplain
